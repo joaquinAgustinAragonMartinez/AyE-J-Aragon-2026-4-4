bec4b6ce7e373182ff2eabf4f0f3783eb794870a
Aragon Joaquin <joaquin.aragonet32@gmail.com>
2026-03-19 11:59:05 -0300

    Add files via upload

---
+++ TP2.cs
@@ -0,0 +1,200 @@
+﻿int a = 25;
+int b = 30;
+if (b > a) {
+    Console.WriteLine("verdadero");
+}
+else {
+    Console.WriteLine("falso");
+}
+
+int c = 14;
+int d = 14;
+if (c <= d) {
+    Console.WriteLine("verdadero");
+}
+else {
+    Console.WriteLine("falso");
+}
+
+int e = 0;
+int f = 0;
+if (e == f)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+int g = 10;
+int h = 10;
+if (g != h)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+int i = 8;
+int j = 3;
+if (i >= j)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+int k = 15;
+int l = 15;
+if (k < l)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+int m = 100;
+int n = 50;
+if (m != n)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+int o = 7;
+float p = 7.0f;
+if (o == p)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+float q = 0.5f;
+int r = 0;
+if (q > r)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+int s = -5;
+int t = -2;
+if (s < t)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+if (5 > 3 && 10 < 20)
+{ 
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+if (8 == 10 || 5!= 2)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+if  (!(10 == 10))
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+if (15 > 10 && !(5 > 10))
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+if (10 + 2 > 10 || 5 * 2 < 8)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+int temperatura = 35;
+bool humedad = false;
+if (temperatura > 30 && humedad==true)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+int edad = 16;
+if (!(edad >= 18)) 
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+if (10 / 3 == 3 && 10 % 3 == 1)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+ string nombre = "ana";
+if (nombre == "juan" || nombre == "ana") 
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+if ((5 * 2) + 10 > 15 && (20 / 4 == 5)
+)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
