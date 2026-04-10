718d3fc5f8ce23fe55e16ec0269d994b1f582081
Aragon Joaquin <joaquin.aragonet32@gmail.com>
2026-04-10 02:26:43 -0300

    Add files via upload

---
+++ Simulacro Evaluación.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("ingrese su nombre");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("ingrese su promedio");
+float promedio = Convert.ToSingle(Console.ReadLine());
+
+Console.WriteLine("ingrese la distancia en kilometros a la que vive");
+int distancia = Convert.ToInt32(Console.ReadLine());
+
+bool DeterminarBeca(float promedio, int distancia)
+{
+    if (promedio >= 8.5 || distancia > 50)
+    {
+        Console.WriteLine("Felicidades " +nombre+ ", tu beca ha sido aprobada");
+        return true;
+    }
+    else
+    {
+        Console.WriteLine("Lo sentimos, no cumples con los requisitos mínimos");
+        return false;
+    }
+}
+DeterminarBeca(promedio, distancia);
