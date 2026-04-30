3f9a13438306719b2a1ce8f82938e75e9fab0323
Aragon Joaquin <joaquin.aragonet32@gmail.com>
2026-04-30 10:26:38 -0300

    Add files via upload

---
+++ trabajo en clase while.cs
@@ -0,0 +1,26 @@
+﻿using System;
+Console.WriteLine("ingrese 0 o 1");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while (num != 0 && num != 1)
+{
+    Console.WriteLine("error, ingrese 0 o 1");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num == 0 || num == 1)
+    {
+        Console.WriteLine("ha ingresado el numero correcto");
+        break;
+    }
+}
+
+Console.WriteLine("ingrese un numero de 2 cifras");
+int num2 = Convert.ToInt32(Console.ReadLine());
+while ((num2 < 10 && num2 > -10) || (num2 > 99 || num2 < -99))
+{
+    Console.WriteLine("error, ingrese nuevamente numero de 2 cifras");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    if (num2 >= 10 && num2 <= 99)
+    {
+        break;
+    }
+}
