6596ea282d33f31eb5eb42ddd548a1590aab0c93
Aragon Joaquin <joaquin.aragonet32@gmail.com>
2026-08-25 13:24:45 -0300

    Add files via upload

---
+++ ejercicio.cs
@@ -0,0 +1,34 @@
+﻿namespace tp_19
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una pakabra o frase");
+            string texto = Console.ReadLine();
+            Invertir(texto);
+
+            string Invertir(string texto)
+            {
+                Stack<char> letras = new Stack<char>();
+
+                foreach (char letra in texto)
+                {
+                    letras.Push(letra);
+                }
+
+                string Invertido = "";
+
+                for (int l = 0; l < texto.Length; l++)
+                {
+                    char ultimaletra = letras.Pop();
+                    Invertido += ultimaletra;
+                }
+                Console.WriteLine($"La palabra invertida es: {Invertido}");
+                return Invertido;
+
+            }
+        }
+
+    }
+}
