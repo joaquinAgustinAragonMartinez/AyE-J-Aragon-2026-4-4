f63ecd1af1cc7f47b607a85947a3faf7261a636f
Aragon Joaquin <joaquin.aragonet32@gmail.com>
2026-08-27 12:02:52 -0300

    Update ejercicio.cs

--- ejercicio.cs
+++ ejercicio.cs
@@ -4,6 +4,7 @@
     {
         public void Main(string[] args)
         {
+            //ejercicio 1
             Console.WriteLine("Ingrese una pakabra o frase");
             string texto = Console.ReadLine();
             Invertir(texto);
@@ -28,6 +29,39 @@
                 return Invertido;
 
             }
+            //ejercicio 4
+string texto = " ";
+int opcion, opcion2;
+Stack<AccionTexto> Acciones = new Stack<AccionTexto>();
+AccionTexto laAcciondelPeek;
+do
+{
+    Console.WriteLine("queres desacer -1 o escribir -2 ?");
+    opcion = Convert.ToInt32(Console.ReadLine());
+    if (opcion == 1)
+    {
+        Acciones.TryPop(out AccionTexto resultado);
+        Acciones.TryPeek(out AccionTexto resultado1);
+        if (resultado1.Contenido == null)
+        {
+            Console.WriteLine("no se epude desacer mas");
+        }
+        else
+        {
+            Console.WriteLine(resultado1.Contenido);
+        }
+    }
+    else
+    {
+        Console.WriteLine("ingresa algo");
+        texto = texto + Console.ReadLine();
+        laAcciondelPeek = new AccionTexto("escritura", texto, "6767676767");
+        Acciones.Push(laAcciondelPeek);
+    }
+    Console.WriteLine("seguimos? si = 1 no = 2");
+    opcion2 = Convert.ToInt32(Console.ReadLine());
+}
+while (opcion2 != 2);
         }
 
     }
