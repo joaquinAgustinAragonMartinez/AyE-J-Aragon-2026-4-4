aaafcc7901bac7dbb89bf76256bd947bcd0f9647
Aragon Joaquin <joaquin.aragonet32@gmail.com>
2026-09-03 10:38:44 -0300

    Add files via upload

---
+++ programa.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Resources;
+using System.Threading.Tasks;
+
+namespace prueba_algoritmos
+{
+    internal class Program
+    {
+        public void volverEnElTiempo(Stack<Personaje> historialDelPersonaje)
+        {
+            Personaje UltimaAccion = historialDelPersonaje.Peek();
+
+            Console.WriteLine("la ultima accion realizada fue :" + UltimaAccion);
+        }
+        public void Golpear(Stack<Personaje> historialDelPersonaje)
+        {
+            string AccionUltima = "Golpe Recibido";
+            int ActualVida = Personaje.VidaActual;
+            ActualVida = ActualVida - 20;
+
+            Personaje VidaActual = historialDelPersonaje.Push(ActualVida);
+
+            Personaje UltimaAccion = historialDelPersonaje.Push(AccionUltima);
+        }
+        static void Main(string[] args)
+        {
+            Personaje personaje1 = new Personaje(100, 100, "talar");
+            Personaje personaje2 = new Personaje(100, 100, "picar");
+            Personaje personaje3 = new Personaje(100, 100, "cocinar");
+
+            volverEnElTiempo();
+            Golpear();
+
+            foreach (Personaje personajes in Personaje)
+            {
+                Console.WriteLine(personaje1);
+                Console.WriteLine(personaje2);
+                Console.WriteLine(personaje3);
+            }
+        }
+    }
+}
---
+++ struct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba_algoritmos
+{
+    public struct Personaje
+    {
+        public int VidaTotal;
+        public int VidaActual;
+        public string UltimaAccion;
+        public Personaje(int VidaTotal, int VidaActual, string UltimaAccion)
+        {
+            this.VidaTotal = VidaTotal;
+            this.VidaActual = VidaActual;
+            this.UltimaAccion = UltimaAccion;
+        }
+    }
+}
