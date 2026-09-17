525a2bb2052f98cac60d592f3fcd174de39b89da
Aragon Joaquin <joaquin.aragonet32@gmail.com>
2026-09-17 10:49:40 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,21 +2,62 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             using var db = new AppDbContext();
 
             //insertar
-
             var Insertar = new productos
             {
+                //Se inserta datos creados en SQL (Menos el id porque es clave primaria y auto_incremet)
+                //EJ:
                 nombre = "fideos",
                 precio = 1500
             };
 
-            db.datos.Add(Insertar);
-            db.SaveChangesAsync();
+            db.Productos.Add(Insertar);
+            db.SaveChanges();
 
+            //Consultar
+            var todos = await db.Productos.ToListAsync();
+            
+
+            //Ejecutar una linea SQL
+            string SQL = "SELECT * for productos where id = 1";
+            var lista = await db.Productos.FromSqlRaw(SQL).ToListAsync();
+            foreach (var d in todos)
+            {
+                Console.WriteLine(d.nombre);
+            }
+
+            //Buscar usuario en especifico
+            int id = 1;
+            var DatoBuscado = Insertar;
+            DatoBuscado = await db.Productos.FindAsync(id);
+            
+            // para que no nos ire error por si no existe un dato
+            if(DatoBuscado != null)
+            {
+                Console.WriteLine(DatoBuscado.nombre);
+            }
+            else
+            {
+                Console.WriteLine("Dato no encontrado con ese ID");
+            }
+
+            if (DatoBuscado != null)
+            {
+                DatoBuscado.nombre = "papa";
+                DatoBuscado.precio = 10000;
+                await db.SaveChangesAsync();
+            }
+
+            //Borrar datos
+            if(DatoBuscado != null)
+            {
+                db.Remove(DatoBuscado);
+                await db.SaveChangesAsync();
+            }
         }
     }
 }
