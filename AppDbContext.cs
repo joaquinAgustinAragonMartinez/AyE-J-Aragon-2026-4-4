3cc25c22a8423a98d0f9e112974b8e6e91576f60
Aragon Joaquin <joaquin.aragonet32@gmail.com>
2026-09-17 11:07:01 -0300

    Update AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -13,10 +13,12 @@ namespace basededatos
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string connectionString = "Server=localhost; Database=mibasededatos; Uid=root; pwd=;";
+            //asegurarse de que el puerto de sql sea 3306 y si es diferente especificar el puerto en el var serverVersion
+            //si hay error usar Port="puerto que esta usando" (ej: Port=3307)
+            
+            string connectionString = "Server=localhost;Port=3307;Database=mibasededatos;Uid=root;Pwd=;";
             var serverVersion = ServerVersion.AutoDetect(connectionString);
             optionsBuilder.UseMySql(connectionString, serverVersion);
-
         }
     }
 }
