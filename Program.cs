139fb65ac78b3a9888e16d1b024218e3f4209678
Aragon Joaquin <joaquin.aragonet32@gmail.com>
2026-09-24 12:00:47 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,34 @@
-﻿namespace tp_23
+namespace tp_23
 {
     public class Program
     {
         static async Task Main(string[] args)
         {
-            using var db = new AppDbContext();
-
+            AppDbContext db = new AppDbContext();
             ArbolPokemon miArbol = new ArbolPokemon();
 
-            var Insertar = new NodoPokemon
-            {
-                
-            };
+            CargarArbolDesdeDB(db, arbol);
 
-            db.nodopokemon.Add(Insertar);
-            db.SaveChanges();
+            bool salir = false;
+            while (!salir)
+            {
+                Console.WriteLine("bienvenido al menu de opciones ingrese el numero de la opcion que quiere elegir");
+                Console.WriteLine("1--consultar base de datos en xampp");
+                Console.WriteLine("2--actualizar pokemon");
+                Console.WriteLine("3--actualizar datos de un pokemon");
+                Console.WriteLine("4--eliminar pokemon");
+                Console.WriteLine("5--sincronizar y reconstruir base de datos");
+                Console.WriteLine("6--hacer busqueda binaria en el arbol");
+                Console.WriteLine("7--salir");
+
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    
+                }
+            }
+
         }
     }
 }
